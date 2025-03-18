b03dbdb05d40cdc1a2b8ed3cc33de85ccd238935
Addams Carvalho - Work <addamscarvalho.sistema@gmail.com>
2025-03-18 16:58:20 -0300

    Commit para ir pra casa.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,11 @@
-const nome = document.querySelector("#name");
+let nome = document.querySelector("#name");
 const email = document.querySelector("#email");
 const unidade = document.querySelector("#unidade");
 const solicitacao = document.querySelector("#solicitacao");
 
-const x = document.querySelector("#enviar")
-
-let mensagem = document.querySelector("#retorno")
-
-
-x.addEventListener("click", mensagemTELA);
 
 
 function mensagemTELA(){
-    retorno.innerHTML = `nome é ${nome}, email é ${email}, unidade é ${unidade} e solicitação é ${solicitacao}.`;
+    retorno.innerHTML = `nome é ${nome}, email é ${email}, unidade é ${unidade} e solicitação é ${solicitacao}`;
+    console.log(retorno);
+}
