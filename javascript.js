39385a8b7aae55755b15dd30408844779a0f33dc
Addams Aprendendo GIT <addamscarvalho.sistema@gmail.com>
2025-03-19 01:15:00 -0300

    Corrigindo o valor das variáveis e posição

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
-var nome = document.querySelector("#name").value;
-var email = document.querySelector("#email").value;
-var unidade = document.querySelector("#unidade").value;
-var solicitacao = document.querySelector("#solicitacao").value;
+
 const botao = document.querySelector("#enviar");
 let verify = document.querySelector("#retorno");
 
 botao.addEventListener("click", mensagemTELA);
 
 function mensagemTELA(){
+    var nome = document.querySelector("#name").value;
+    var email = document.querySelector("#email").value;
+    var unidade = document.querySelector("#unidade").value;
+    var solicitacao = document.querySelector("#solicitacao").value;
     verify.innerHTML = `nome é ${nome}, email é ${email}, unidade é ${unidade} e solicitação é ${solicitacao}`;
 }
