62b8246795f8c7c63a32c09592c42763e779f02f
Addams Aprendendo GIT <addamscarvalho.sistema@gmail.com>
2025-03-19 00:16:43 -0300

    Commit pra ir dormir e tentar amanhã dnovo.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
-let nome = document.querySelector("#name");
-const email = document.querySelector("#email");
-const unidade = document.querySelector("#unidade");
-const solicitacao = document.querySelector("#solicitacao");
+var nome = document.querySelector("#name").value;
+var email = document.querySelector("#email").value;
+var unidade = document.querySelector("#unidade").value;
+var solicitacao = document.querySelector("#solicitacao").value;
+const botao = document.querySelector("#enviar");
+let verify = document.querySelector("#retorno");
 
-
+botao.addEventListener("click", mensagemTELA);
 
 function mensagemTELA(){
-    retorno.innerHTML = `nome é ${nome}, email é ${email}, unidade é ${unidade} e solicitação é ${solicitacao}`;
-    console.log(retorno);
+    verify.innerHTML = `nome é ${nome}, email é ${email}, unidade é ${unidade} e solicitação é ${solicitacao}`;
 }
