d627f44ed135c3605a6cd408c1742a89ae39a9b8
Addams Aprendendo GIT <addamscarvalho.sistema@gmail.com>
2025-03-23 02:02:35 -0300

    Mais organizações e adicionando README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+Hoje é 23/03/2025.
+
+Esse projetinho sem nome faz parte dos meus estudos práticos.
+Levei cerca de 7 horas pra construí-lo, enquando estudava algumas funções que não conhecia.
+Esse sistema simula abertura de um chamado(solicitação) para determinado tipo de serviço, onde o usuário insere seu nome, email e unidade qual deseja atendimento.
+
+Ao começar meus estudos em back-end e banco de dados, continuarei incrementando novas funções. Alguns que já pensei são: número do chamado, exibição de chamados de determinado usuário e se o chamado está aberto ou concluído. Terá uma organização auditável dos chamados, da abertura ao encerramento.
+Além disso, posso fazer atualizações das variáveis para trabalhar tudo como OBJETO(JSON) para facilitar o manuseio das variáveis.
+
+Irei atualizando esse README.md com "Att - data do dia: texto" para manter um histórico didático das etapas da minha aprendizagem..
+
+Tudo também está sendo registrados em GIT.
--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,21 +1,23 @@
 
-const botao = document.querySelector("#enviar");
-let verify = document.querySelector("#retorno");
+const botao = document.querySelector("#enviar"); /*Cria botao para receber a tag button*/
+let verify = document.querySelector("#retorno");/* cria para receber a tag div*/
 
-botao.addEventListener("click", mensagemTELA);
+botao.addEventListener("click", mensagemTELA); /* executa função ao clicar em botao*/
 
 function mensagemTELA(){
-    var nome = document.querySelector("#name").value;
-    var email = document.querySelector("#email").value;
-    var unidade = document.querySelector("#unidade").value;
-    var solicitacao = document.querySelector("#solicitacao").value;
+    var nome = document.querySelector("#name").value; /*recebe input*/
+    var email = document.querySelector("#email").value; /*recebe input*/
+    var unidade = document.querySelector("#unidade").value; /*recebe input*/
+    var solicitacao = document.querySelector("#solicitacao").value; /*recebe input*/
 
     if (!nome || !email || !unidade || !solicitacao){
-        verify.innerHTML = "Por favor, preencha todos os campos!!";
+        verify.innerHTML = "Por favor, preencha todos os campos!!"; /*Exibe mensagem de vazio, de houver*/
     }
-    else {   
-        alert(`Olá, ${nome}, sua solicitação "${solicitacao}" foi registrada em nosso sistema.`);
-        window.location.href = "confirma.html";
+    else {  
+        /*Quando eu criar um banco de dados para inserir as infos,
+        irei adicionar um IF nessa linha*/
+        alert(`Olá, ${nome}, sua solicitação "${solicitacao}" foi registrada em nosso sistema.`); /*Alerta sucesso*/
+        window.location.href = "confirma.html"; /*Encaminha pra a página final de confirmação*/
     }
 
 }
